e6039a8760d82d83f2a566ddc298afdaa7a892ca
Sami Ollila <ollila.sami93@gmail.com>
2020-02-20 10:56:36 +0200

    Completed 3.15

--- index.js
+++ index.js
@@ -60,11 +60,15 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.delete(`/api/persons/:id`, (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.filter(person => person.id !== id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+  PhoneNumber.findByIdAndRemove(req.params.id)
+    .then(result => {
+      console.log(`${req.params.id} removed.`)
+      res.status(204).end()
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).end()
+    })
 })
 
 app.post('/api/persons', (req, res) => {
